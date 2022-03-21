3452a9d67489aa2852b86b68aa5ac937f52700cc
celinebenetta <66906328+celinebenetta@users.noreply.github.com>
2022-03-22 00:32:23 +0800

    Update AppStoreClean.sql

--- sql/AppStoreClean.sql
+++ sql/AppStoreClean.sql
@@ -4,6 +4,9 @@
 
 *******************/
 
-DROP TABLE IF EXISTS downloads;
-DROP TABLE IF EXISTS games;
-DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS calculators;
+DROP TABLE IF EXISTS loan;
+DROP TABLE IF EXISTS students;
+DROP TABLE IF EXISTS test_students;
+DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS department;
