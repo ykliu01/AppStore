6376c4fcea282f9802e7bf95fed9247694f4b587
ykliu01 <101163069+ykliu01@users.noreply.github.com>
2022-03-24 14:19:03 +0800

    Update add_sql.sh

--- add_sql.sh
+++ add_sql.sh
@@ -31,7 +31,7 @@ URI="postgres://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
 psql ${URI} -f sql/AppStoreClean.sql
 psql ${URI} -f sql/AppStoreSchema.sql
 psql ${URI} -f sql/AppStoreTestStudents.sql
-psql ${URI} -f sql/students.sql
 psql ${URI} -f sql/MRT.sql
+psql ${URI} -f sql/students.sql
 psql ${URI} -f sql/calculators.sql
 psql ${URI} -f sql/loan.sql
