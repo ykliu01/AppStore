d5e925f3f4bbee6d813925a3469083c82ccd174c
ykliu01 <101163069+ykliu01@users.noreply.github.com>
2022-03-25 15:24:31 +0800

    Update urls.py

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -30,8 +30,8 @@ urlpatterns = [
     path('myCalculators/<str:id>',app.views.myCalculators, name='myCalculators'),
     path('editAvailability/<str:id>',app.views.editAvailability, name='editAvailability'),
     path('hot', app.views.hot, name='hot'),
-    path('myCalculators/<str::id>/addCalculator', app.views.addCalculator, name='addCalculator'),
-    path('findCalculators/<str:id>/findCalculators', app.views.findCalculators, name='findCalculators')
+    #path('myCalculators/<str::id>/addCalculator', app.views.addCalculator, name='addCalculator'),
+    #path('findCalculators/<str:id>/findCalculators', app.views.findCalculators, name='findCalculators')
 ]
 
 """"path('add', app.views.add, name='add'),"""
