7a52368a203bffbabc07fd5a1e217d7754053ee4
celinebenetta <66906328+celinebenetta@users.noreply.github.com>
2022-03-30 15:02:17 +0800

    Update AppStoreTestStudents.sql

--- sql/AppStoreTestStudents.sql
+++ sql/AppStoreTestStudents.sql
@@ -1,8 +1,8 @@
-INSERT INTO test_students VALUES ( '1','930','1');
-INSERT INTO test_students VALUES ( '2','1000','2');
-INSERT INTO test_students VALUES ( '3','1130','3');
-INSERT INTO test_students VALUES ( '4','1230','5');
-INSERT INTO test_students VALUES ( '5','1230','5');
-INSERT INTO test_students VALUES ( '6','1300','7');
-INSERT INTO test_students VALUES ( '7','1400','7');
-INSERT INTO test_students VALUES ( '8','1500','7');
+INSERT INTO test_students VALUES ( '1',930,'1');
+INSERT INTO test_students VALUES ( '2',1000,'2');
+INSERT INTO test_students VALUES ( '3',1130,'3');
+INSERT INTO test_students VALUES ( '4',1230,'5');
+INSERT INTO test_students VALUES ( '5',1230,'5');
+INSERT INTO test_students VALUES ( '6',1300,'7');
+INSERT INTO test_students VALUES ( '7',1400,'7');
+INSERT INTO test_students VALUES ( '8',1500,'7');
