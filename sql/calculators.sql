12436f515e55026fee4240422c88c2c7d0e7e6a4
celinebenetta <66906328+celinebenetta@users.noreply.github.com>
2022-03-30 15:56:37 +0800

    Update calculators.sql

--- sql/calculators.sql
+++ sql/calculators.sql
@@ -1,2 +1,2 @@
-INSERT INTO calculators VALUES ('texas', '1a2b', 'graphing', 100, 'good', 'available', 'mfiddian0');
+INSERT INTO calculators VALUES ('texas', '1a2b', 'graphing', 100, 'good', 'available', 'kmacvay0@shinystat.com');
 
