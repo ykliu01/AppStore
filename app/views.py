bbd235e411d2da3c9cabdd59fa059b946e0ea772
YeOldMan23 <kieren.chua23@gmail.com>
2022-04-03 19:37:38 +0800

    add cal attempt bug

--- app/views.py
+++ app/views.py
@@ -169,7 +169,7 @@ def addCalculator(request, id):
                 ##TODO: date validation
                 cursor.execute("INSERT INTO calculators VALUES (%s, %s, %s, %s, %s, %s, %s)"
                         ,[request.POST['brand'] , request.POST['serial_number'], request.POST['calc_type'], request.POST['price'],
-                           request.POST['calc_condition'] , request.POST['availability'], [id]])
+                           request.POST['calc_condition'] , request.POST['availability'], id])
                 return redirect('index')
             else:
                 status = 'Calculator with serial number %s already exists' % (request.POST['serial_number'])
