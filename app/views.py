72b72245d29f301ff8f3e705d78df771722f8727
celinebenetta <66906328+celinebenetta@users.noreply.github.com>
2022-04-07 00:49:22 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -158,6 +158,12 @@ def edit(request, id):
 
 # Select all customrs from cetrain id
 def myCalculators(request, id):
+    
+    if request.POST:
+        if request.POST['action'] == 'delete':
+            with connection.cursor() as cursor:
+                cursor.execute("DELETE FROM calculators WHERE serial_number = %s", [request.POST['id']])
+    
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM calculators cal WHERE cal.email = %s", [id])
         calculator = cursor.fetchall()
