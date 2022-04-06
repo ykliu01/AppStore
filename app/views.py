13e369079fa1dd50da6b33639de75170c2eb8bbc
jinggadavid <60139783+jinggadavid@users.noreply.github.com>
2022-04-07 02:53:16 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -244,20 +244,10 @@ def findCalculators(request):
     result_dict={}
     if request.POST:
         with connection.cursor() as cursor:
-            select_statement = "SELECT c.brand, c.serial_number, c.price, c.calc_condition, l.location_name, s.time_availability, s.first_name, s.last_name, s.email FROM calculators c, students s, locations l WHERE c.availability='available' AND c.email = s.email AND l.location_id=s.location_id AND s.time_availability=CAST(%s as INTEGER) AND l.location_name = %s AND c.calc_type=%s"
+            select_statement = "SELECT c.brand, c.serial_number, c.price, c.calc_condition, l.location_name, s.time_availability, s.first_name, s.last_name, s.email FROM calculators c, students s, locations l WHERE c.availability='available' AND c.email = s.email AND l.location_id=s.location_id AND (CAST(%s as INTEGER)-s.time_availability<=59) AND l.location_name = %s AND c.calc_type=%s"
             user_input = (request.POST['s.time_availability'], request.POST['l.location_name'], request.POST['c.calc_type'])
-            
-            if user_input[0]==2459 and user_input[1]=='ANY' and user_input[2]=='ANY':
-                select_statement = "SELECT c.brand, c.serial_number, c.price, c.calc_condition, l.location_name, s.time_availability, s.first_name, s.last_name, s.email FROM calculators c, students s, locations l WHERE c.availability='available' AND c.email = s.email AND l.location_id=s.location_id AND (CAST(%s as INTEGER)<=2359) AND l.location_name<>%s AND c.calc_type<>%s"
-                cursor.execute(select_statement,user_input)
-                available_calculators = cursor.fetchall()
-           
-            else:
-                select_statement = "SELECT c.brand, c.serial_number, c.price, c.calc_condition, l.location_name, s.time_availability, s.first_name, s.last_name, s.email FROM calculators c, students s, locations l WHERE c.availability='available' AND c.email = s.email AND l.location_id=s.location_id AND (CAST(%s as INTEGER)-s.time_availability<=59) AND l.location_name = %s AND c.calc_type=%s"
-                user_input = (request.POST['s.time_availability'], request.POST['l.location_name'], request.POST['c.calc_type'])
-                cursor.execute(select_statement,user_input)
-                available_calculators = cursor.fetchall()
-                
+            cursor.execute(select_statement,user_input)
+            available_calculators = cursor.fetchall()    
         result_dict = {'Results':available_calculators}
         return render(request, 'app/findCalculators.html', result_dict)
     return render(request,'app/findCalculators.html', result_dict)
