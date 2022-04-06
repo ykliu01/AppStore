b02a7f053c0c479f87c4a6f1f6d1eb4329977378
ykliu01 <101163069+ykliu01@users.noreply.github.com>
2022-04-06 19:08:11 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from urllib import request
 from django.shortcuts import render, redirect
 from django.db import connection
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
