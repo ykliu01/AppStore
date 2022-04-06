e349cd1a02a0026386824c9e3c5ee1fdeb272e9b
jinggadavid <60139783+jinggadavid@users.noreply.github.com>
2022-04-07 02:41:58 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -197,7 +197,7 @@ def findCalculators_time(request):
     if request.POST:
         with connection.cursor() as cursor:
             select_statement = "SELECT c.calc_type, c.brand, c.serial_number, c.price, c.calc_condition, l.location_name, s.time_availability, s.first_name, s.last_name, s.email FROM calculators c, students s, locations l WHERE c.availability='available' AND c.email = s.email AND l.location_id=s.location_id AND (CAST(%s as INTEGER)-s.time_availability<=59)"
-            user_input = request.POST['s.time_availability']
+            user_input = (request.POST['s.time_availability'])
             cursor.execute(select_statement,user_input)
             available_calculators = cursor.fetchall() 
         result_dict = {'Results':available_calculators}
