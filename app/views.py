a29451f75024014ccd6ddfd27e2149036b23ce03
jinggadavid <60139783+jinggadavid@users.noreply.github.com>
2022-04-07 02:34:51 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -196,7 +196,7 @@ def findCalculators_time(request):
     result_dict={}
     if request.POST:
         with connection.cursor() as cursor:
-            select_statement = "SELECT c.calc_type, c.brand, c.serial_number, c.price, c.calc_condition, l.location_name, s.time_availability, s.first_name, s.last_name, s.email FROM calculators c, students s, locations l WHERE c.availability='available' AND c.email = s.email AND l.location_id=s.location_id AND CAST(%s as INTEGER)-s.time_availability<=59"
+            select_statement = "SELECT c.calc_type, c.brand, c.serial_number, c.price, c.calc_condition, l.location_name, s.time_availability, s.first_name, s.last_name, s.email FROM calculators c, students s, locations l WHERE c.availability='available' AND c.email = s.email AND l.location_id=s.location_id AND (CAST(%s as INTEGER)-s.time_availability<=59)"
             user_input = request.POST['s.time_availability']
             cursor.execute(select_statement,user_input)
             available_calculators = cursor.fetchall() 
@@ -204,6 +204,7 @@ def findCalculators_time(request):
         return render(request, 'app/findCalculators_time.html', result_dict)
     return render(request,'app/findCalculators_time.html', result_dict)
 
+'''
 def findCalculators_location(request):
     result_dict={}
     if request.POST:
@@ -272,3 +273,4 @@ def findCalculators_all(request):
         available_calculators = cursor.fetchall() 
     result_dict = {'Results':available_calculators}
     return render(request, 'app/findCalculators_all.html', result_dict)
+'''
