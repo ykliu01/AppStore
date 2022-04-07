fe5e8d9197385648923360a9272a4823e55b8315
celinebenetta <66906328+celinebenetta@users.noreply.github.com>
2022-04-08 03:38:57 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -697,31 +697,8 @@ def findCalculators_all(request):
     return render(request, 'app/findCalculators_all.html', result_dict)
 
 def settings(request, id):
-    # dictionary for initial data with
-    # field names as keys
-    context ={}
-
-    # fetch the object related to passed id
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM students WHERE email = %s", [id])
-        obj = cursor.fetchone()
-
-    status = ''
-    # save the data from the form
-
-    if request.POST:
-        with connection.cursor() as cursor:
-            cursor.execute("UPDATE students SET first_name = %s, last_name = %s, time_availability = %s, location_id = %s WHERE email = %s"
-                    , [request.POST['first_name'], request.POST['last_name'],
-                        request.POST['time_availability'] , request.POST['location_id'], id ])
-            status = 'Student edited successfully!'
-            cursor.execute("SELECT * FROM students WHERE email = %s", [id])
-            obj = cursor.fetchone()
-
-    context["obj"] = obj
-    context["status"] = status
  
-    return render(request, "app/edit.html", context)
+    return render(request, "app/settings.html", context)
 
 
 def logout(request):
