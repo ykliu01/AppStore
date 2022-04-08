42b70156a558d3904153b8542497359c5e0641ca
ykliu01 <101163069+ykliu01@users.noreply.github.com>
2022-04-08 11:38:56 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -712,9 +712,7 @@ def settings(request):
     context["obj"] = obj
      if request.POST:
         with connection.cursor() as cursor:
-                cursor.execute("UPDATE students SET first_name = %s, last_name = %s, time_availability = %s, location_id = %s WHERE email = %s"
-                    , [request.POST['first_name'], request.POST['last_name'],
-                        request.POST['time_availability'] , request.POST['location_id'], [username]])
+            cursor.execute("UPDATE students SET first_name = %s, last_name = %s, time_availability = %s, location_id = %s WHERE email = %s", [request.POST['first_name'], request.POST['last_name'],request.POST['time_availability'] , request.POST['location_id'], [username]])
     
     return render(request, "app/settings.html", context)
 
